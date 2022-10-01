6b2ac7192cf5605a0b5c72b2dd16bb1a10852a18
GaliullinAR <galiullin.ar651@gmail.com>
2022-10-01 22:41:21 +0300

    добавил слайдер

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   
 
-
+  const swiper = new Swiper('.showcase-carousel', {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800,
+    centeredSlides: true
+  })
 
 
   document.querySelector('.showcase__video').playbackRate = 2;
